c1c62c8feee11889d7eabd956da06165d98821b4
David Huggins-Daines <dhd@ecolingui.ca>
2024-06-17 11:50:48 -0400

    fix: oops use the public server

--- js/config.ts
+++ js/config.ts
@@ -1,6 +1,6 @@
 export const ALEXI_URL = "https://dhdaines.github.io/alexi";
-//export const ZONALDA_API_URL = "https://zonalda.ecolingui.ca/api";
-export const ZONALDA_API_URL = "http://localhost:8000/api";
+export const ZONALDA_API_URL = "https://zonalda.ecolingui.ca/api";
+//export const ZONALDA_API_URL = "http://localhost:8000/api";
 export const COLLECTES_URL = "https://ville.sainte-adele.qc.ca/upload/images/SA-calendrier-collecte-2023-2024-8-5x11-vertical-CROP.jpg";
 export const COLLECTE_ZONE_URL = {
   jaune: "https://lespaysdenhaut.com/wp-content/uploads/2023/07/SainteAdele_jaune2023-24.pdf",
