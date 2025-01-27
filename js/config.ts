c4796c12d112d2581bc1a1588408bcecccc38338
David Huggins-Daines <dhd@ecolingui.ca>
2025-01-27 08:36:03 -0500

    fix: get alexi url right

--- js/config.ts
+++ js/config.ts
@@ -1,4 +1,4 @@
-export const ALEXI_URL = "https://dhdaines.github.io/alexi";
+export const ALEXI_URL = "https://dhdaines.github.io/alexi/vdsa";
 export const ZONALDA_API_URL = "https://zonalda.ecolingui.ca/api";
 //export const ZONALDA_API_URL = "http://localhost:8000/api";
 export const COLLECTES_URL = "https://ville.sainte-adele.qc.ca/upload/images/SA-calendrier-collecte-2023-2024-8-5x11-vertical-CROP.jpg";
